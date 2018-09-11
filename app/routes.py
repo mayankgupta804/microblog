d6b0204b428be8764bf08aac95ac98d72f5205ec
mayank <mayank.gupta804@gmail.com>
2018-09-12 02:39:47 +0530

    Add jinja templates + play around with sending data over routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = {"username": "Mayank"}
+    posts = [
+        {
+            'author':{'username':'Gaurav'},
+            'body': 'Beautiful day in Bangalore!'
+        },
+        {
+            'author':{'username':'Abhishek'},
+            'body': 'Stree was a cool movie!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts) 
