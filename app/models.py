2ede24e71b89a13d25d7e844d7737db53ecd27da
mayank <mayank.gupta804@gmail.com>
2018-10-16 01:16:31 +0530

    Add new feature to edit user profile

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
-from . import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
+
+from . import db
 from app import login
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +13,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(64))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow )
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -18,7 +23,11 @@ class User(UserMixin, db.Model):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)        
+        return check_password_hash(self.password_hash, password)     
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,7 +36,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Post {}'.format(self.body)
+        return '<Post {}>'.format(self.body)
 
 @login.user_loader
 def load_user(id):
